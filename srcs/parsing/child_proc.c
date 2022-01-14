8b09c551ce19b6fad50d83749ff9b9780b094488
Askeladd42 <piema.lam@gmail.com>
2022-01-14 14:49:36 +0100

    starting the project structure

--- srcs/parsing/child_proc.c
+++ srcs/parsing/child_proc.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   child_proc.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/03 09:33:09 by plam              #+#    #+#             */
-/*   Updated: 2022/01/14 14:30:42 by plam             ###   ########.fr       */
+/*   Created: 2022/01/14 14:47:52 by plam              #+#    #+#             */
+/*   Updated: 2022/01/14 14:48:54 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	pipex(int f1, int f2, char *cmd1, char *cmd2)
+void	child_process(int f1, char *cmd1)
 {
-	int		end[2];
-	pid_t	prt;
 
-	pipe(end);
-	prt = fork();
-	if (prt < 0)
-		return (perror("Fork: "));
-	if (!prt)
-		child_process(f1, cmd1);
-	else
-		parent_process(f2, cmd2);
 }
--- srcs/parsing/parent_proc.c
+++ srcs/parsing/parent_proc.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parent_proc.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/03 09:33:09 by plam              #+#    #+#             */
-/*   Updated: 2022/01/14 14:30:42 by plam             ###   ########.fr       */
+/*   Created: 2022/01/14 14:47:36 by plam              #+#    #+#             */
+/*   Updated: 2022/01/14 14:49:34 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	pipex(int f1, int f2, char *cmd1, char *cmd2)
+void	parent_process(int f2, char *cmd2)
 {
-	int		end[2];
-	pid_t	prt;
-
-	pipe(end);
-	prt = fork();
-	if (prt < 0)
-		return (perror("Fork: "));
-	if (!prt)
-		child_process(f1, cmd1);
-	else
-		parent_process(f2, cmd2);
+	
 }
