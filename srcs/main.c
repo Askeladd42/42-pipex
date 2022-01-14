853ec8d9c74ae095b0d2516d7e4ab91057e2bdf1
Askeladd42 <piema.lam@gmail.com>
2022-01-14 16:42:07 +0100

    starting the project structure & psseudo code for the functions used for it

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 09:33:09 by plam              #+#    #+#             */
-/*   Updated: 2022/01/14 15:04:58 by plam             ###   ########.fr       */
+/*   Updated: 2022/01/14 16:42:02 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int main(int ac, char **av, char **envp)
 {
 	int f1;
 	int f2;
-	f1 = open(ag[1], O_RDONLY);
-	f2 = open(ag[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	f1 = open(av[1], O_RDONLY);
+	f2 = open(av[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (f1 < 0 || f2 < 0)
 		return (-1);
 	pipex(f1, f2, av, envp);
