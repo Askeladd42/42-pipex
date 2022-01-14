d1e82a6c9436d3cfd779017d45cc09a8d295de19
Askeladd42 <piema.lam@gmail.com>
2022-01-14 15:19:10 +0100

    starting the project structure

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 15:04:23 by plam              #+#    #+#             */
-/*   Updated: 2022/01/14 15:04:53 by plam             ###   ########.fr       */
+/*   Updated: 2022/01/14 15:18:20 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 void	pipex(int f1, int f2, char *cmd1, char *cmd2)
 {
 	int		end[2];
+	int		status;
 	pid_t	prt;
+	pid_t	chd;
 
 	pipe(end);
 	prt = fork();
@@ -23,6 +25,9 @@ void	pipex(int f1, int f2, char *cmd1, char *cmd2)
 		return (perror("Fork: "));
 	if (!prt)
 		child_process(f1, cmd1);
-	else
+	chd = fork();
+	if (chd < 0)
+		return (perror("Fork: "));
+	if (!chd)
 		parent_process(f2, cmd2);
 }
