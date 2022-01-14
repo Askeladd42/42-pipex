89a522bcbc54f26c9b80db6470ad919bfd554143
Askeladd42 <piema.lam@gmail.com>
2022-01-14 16:28:17 +0100

    starting the project structure & psseudo code for the functions used for it

--- headers/pipex.h
+++ headers/pipex.h
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 09:33:34 by plam              #+#    #+#             */
-/*   Updated: 2022/01/14 16:00:56 by plam             ###   ########.fr       */
+/*   Updated: 2022/01/14 16:28:08 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,17 @@
 # include <string.h>
 # include <fcntl.h>
 # include <sys/wait.h>
+# include <sys/errno.h>
 
+# define ERR -1
+# define IN 0
+# define OUT 1
+
+size_t	ft_strlen(const char *s);
 char	**ft_split(char const *s, char c);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 int		child_process(int f1, char *cmd1);
 int		parent_process(int f2, char *cmd2);
