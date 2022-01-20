44efbe2c511ebd4a23fd5b1adf6a40b0772284e3
Askeladd42 <piema.lam@gmail.com>
2022-01-20 14:46:41 +0100

    going on the idea to use 2 children process instead of the parent/child process for pipex execution

--- srcs/parsing/path_parsing.c
+++ srcs/parsing/path_parsing.c
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 16:06:01 by plam              #+#    #+#             */
-/*   Updated: 2022/01/20 14:15:18 by plam             ###   ########.fr       */
+/*   Updated: 2022/01/20 14:46:34 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,24 @@
 ** if not, use open(path, O_CREAT).
 */
 
-int	path_parsing(char *path)
+int	path_parsing(char *path, t_ppx *ppx)
 {
 	char *PATH_from_envp;
 	char **mypaths;
 	char **mycmdargs;// retrieve the line PATH from envp
-	PATH_from_envp = ft_substr(envp ....);
+	char *cmd;
+	int i;
+
+	PATH_from_envp = ft_substr(ppx->envp);
 	mypaths = ft_split(PATH_from_envp, ":"); // see section 4 for a small note[0]
-	mycmdargs = ft_split(ag[2], " ");// in your child or parent processint  i;
-	char *cmd;i = -1;
+	mycmdargs = ft_split(ppx->av[2], " ");// in your child or parent processint  i;
+	i = -1;
 	while (mypaths[++i])
 	{
-		cmd = ft_join(mypaths[i], ag[2]); // protect your ft_join
-		execve(cmd, mycmdargs, envp); // if execve succeeds, it exits
+		cmd = ft_strjoin(mypaths[i], ppx->av[2]); // protect your ft_join
+		execve(cmd, mycmdargs, ppx->envp); // if execve succeeds, it exits
 		// perror("Error"); <- add perror to debug
-		free(cmd) // if execve fails, we free and we try a new path
+		free(cmd); // if execve fails, we free and we try a new path
 	}
 	return (EXIT_FAILURE);
 }
