a0170daeccfc0e54bba2993bc59f3bb9f7fe509c
Askeladd42 <piema.lam@gmail.com>
2022-01-20 15:03:25 +0100

    cmd verification done

--- srcs/cmd_funcs/cmd_verification.c
+++ srcs/cmd_funcs/cmd_verification.c
@@ -6,18 +6,19 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 12:48:03 by plam              #+#    #+#             */
-/*   Updated: 2022/01/20 15:01:44 by plam             ###   ########.fr       */
+/*   Updated: 2022/01/20 15:03:20 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+/* using access to the command used to see if it exists : return OK if it
+** exists, ERR if it isn't.
+*/
+
 int	cmd_verification(char *cmd)
 {
-	/* using access to the command used to see if it exists : return OK if it
-	** exists, ERR if it isn't.
-	*/
-	int ret;
+	int	ret;
 
 	ret = access(cmd, X_OK);
 	if (ret == -1)
