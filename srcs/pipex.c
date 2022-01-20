0c6ae79ef1acc3f61d2cee959c41e34726f00052
Askeladd42 <piema.lam@gmail.com>
2022-01-20 12:46:18 +0100

    going on the idea to use 2 children process instead of the parent/child process for pipex execution

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 15:04:23 by plam              #+#    #+#             */
-/*   Updated: 2022/01/14 16:10:09 by plam             ###   ########.fr       */
+/*   Updated: 2022/01/20 12:45:25 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,22 @@ void	pipex(int f1, int f2, char *cmd1, char *cmd2)
 {
 	int		end[2];
 	int		status;
-	pid_t	prt;
-	pid_t	chd;
+	pid_t	ch1;
+	pid_t	ch2;
 
 	pipe(end);
-	prt = fork();
-	if (prt < 0)
+	ch1 = fork();
+	if (ch1 < 0)
 		return (perror("Fork: "));
-	if (!prt)
+	if (!ch1)
 		child_process(f1, cmd1);
-	chd = fork();
-	if (chd < 0)
+	ch2 = fork();
+	if (ch2 < 0)
 		return (perror("Fork: "));
-	if (!chd)
-		parent_process(f2, cmd2);
+	if (!ch2)
+		child_process(f2, cmd2);
 	close(end[0]);
 	close(end[1]);
-	waitpid(prt, &status, 0);
-	waitpid(chd, &status, 0);
+	waitpid(ch1, &status, 0);
+	waitpid(ch2, &status, 0);
 }
