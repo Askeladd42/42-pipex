4d4ecff073e159b9330c76fa9d8931a89cb07996
Askeladd42 <piema.lam@gmail.com>
2022-01-24 13:28:13 +0100

    open & closing files functions added

--- srcs/parsing/open_close.c
+++ srcs/parsing/open_close.c
@@ -6,29 +6,24 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 13:13:43 by plam              #+#    #+#             */
-/*   Updated: 2022/01/24 13:16:05 by plam             ###   ########.fr       */
+/*   Updated: 2022/01/24 13:28:06 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	close_file(int file)
+int	close_file(t_ppx *ppx)
 {
-	close(file);
+	close(ppx->infile);
+	close(ppx->outfile);
 	return (0);
 }
 
 
 int	open_file(t_ppx *ppx)
 {
-	int	file;
-
-	file = 0;
-	if (i == 1)
-		file = open(ppx->av, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-	else if (i == 2)
-		file = open(ppx->av, O_RDONLY, 0777);
-	if (file == -1)
-		error("open problem", "");
-	return (file);
+	ppx->infile = open(ppx->av[1], O_RDONLY);
+	ppx->outfile = open(ppx->av[ppx->ac - 1],
+				O_CREAT | O_WRONLY | O_TRUNC, 0777);
+	return (0);
 }
