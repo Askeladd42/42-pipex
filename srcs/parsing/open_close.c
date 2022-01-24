992494bb39e270df97ffecd8613fcb12fa1250e0
Askeladd42 <piema.lam@gmail.com>
2022-01-24 13:30:43 +0100

    open & closing files functions added

--- srcs/parsing/open_close.c
+++ srcs/parsing/open_close.c
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 13:13:43 by plam              #+#    #+#             */
-/*   Updated: 2022/01/24 13:28:06 by plam             ###   ########.fr       */
+/*   Updated: 2022/01/24 13:30:40 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,9 @@ int	open_file(t_ppx *ppx)
 	ppx->infile = open(ppx->av[1], O_RDONLY);
 	ppx->outfile = open(ppx->av[ppx->ac - 1],
 				O_CREAT | O_WRONLY | O_TRUNC, 0777);
+	if (ppx->outfile == -1)
+		return (perror);
+	if (ppx->infile == -1)
+		return (perror); //change the perror with the right function	
 	return (0);
 }
