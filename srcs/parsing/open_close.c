cba65167d6a829e41d6363369eb1c034e50e7dd5
plam <plam@student.42.fr>
2022-01-25 00:11:29 +0100

    put perror functions and open & close files

--- srcs/parsing/open_close.c
+++ srcs/parsing/open_close.c
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 13:13:43 by plam              #+#    #+#             */
-/*   Updated: 2022/01/25 00:04:58 by plam             ###   ########.fr       */
+/*   Updated: 2022/01/25 00:10:48 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@ int	open_file(t_ppx *ppx)
 {
 	ppx->infile = open(ppx->av[1], O_RDONLY);
 	if (ppx->infile == -1)
-		ex_perror(ppx->av[ppx->ac - 1], ppx->infile);
-	ppx->outfile = open(ppx->av[ppx->ac - 1],
-				O_CREAT | O_WRONLY | O_TRUNC, 0777);
-	if (ppx->outfile == -1)
 	{
 		ft_putstr_fd("0\n", ppx->outfile);
 		ex_perror(ppx->av[1], 0);
 	}
+	ppx->outfile = open(ppx->av[ppx->ac - 1],
+				O_CREAT | O_WRONLY | O_TRUNC, 0777);
+	if (ppx->outfile == -1)
+		ex_perror(ppx->av[ppx->ac - 1], ppx->infile);
 	return (0);
 }
