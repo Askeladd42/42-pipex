702f1b04dcd0e981b429120dc7113c8158edfdbb
Askeladd42 <piema.lam@gmail.com>
2022-01-24 13:17:24 +0100

    open & closing files functions added

--- srcs/errors.c
+++ srcs/errors.c
@@ -6,25 +6,11 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 12:34:03 by plam              #+#    #+#             */
-/*   Updated: 2022/01/24 12:58:58 by plam             ###   ########.fr       */
+/*   Updated: 2022/01/24 13:17:22 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-int	open_file(char *argv, int i)
-{
-	int	file;
-
-	file = 0;
-	if (i == 1)
-		file = open(argv, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-	else if (i == 2)
-		file = open(argv, O_RDONLY, 0777);
-	if (file == -1)
-		error("open problem", "");
-	return (file);
-}
 
 void	error(char *s, char *argv)
 {
