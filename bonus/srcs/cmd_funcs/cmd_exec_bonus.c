ee747e8f9c800440a2a6bd1a6aa5ce1a56642338
Askeladd42 <piema.lam@gmail.com>
2022-02-02 18:41:10 +0100

    bonus exception to solve

--- bonus/srcs/cmd_funcs/cmd_exec_bonus.c
+++ bonus/srcs/cmd_funcs/cmd_exec_bonus.c
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 11:36:18 by plam              #+#    #+#             */
-/*   Updated: 2022/02/02 17:43:35 by plam             ###   ########.fr       */
+/*   Updated: 2022/02/02 18:40:11 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,15 @@ void	cmd_exec(char *av, char **envp, t_ppx *ppx)
 	cmd = ft_split(av, ' ');
 	cmd_path = path_parsing(cmd[0], envp);
 	if (cmd_path == NULL)
-		execve(cmd[0], cmd, envp);
+	{
+		if (execve(cmd[0], cmd, envp) == ERR)
+			close(ppx->pipe[R_END]);
+	}
 	else
-		execve(cmd_path, cmd, envp);
+	{
+		if (execve(cmd_path, cmd, envp) == ERR)
+			close(ppx->pipe[R_END]);
+	}
 	if (cmd_path)
 		free(cmd_path);
 	fr_tab(cmd);
