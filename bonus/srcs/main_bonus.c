7f8e00344ddb35506d13c26679ebc200e41e6fa9
Askeladd42 <piema.lam@gmail.com>
2022-02-02 15:50:18 +0100

    makefile issue solved

--- bonus/srcs/main_bonus.c
+++ bonus/srcs/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 09:33:09 by plam              #+#    #+#             */
-/*   Updated: 2022/02/02 15:22:43 by plam             ###   ########.fr       */
+/*   Updated: 2022/02/02 15:49:59 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	main(int ac, char **av, char **envp)
 		init_ppx(&ppx, ac, av, envp);
 		i = 2;
 		infile = open_file(ppx.av[1], 2);
-		outfile = open_file(ppx.av[ac - 1], 1);
+		outfile = open_file(ppx.av[ac - 1], 3);
 		dup2(infile, STDIN_FILENO);
 		while (ppx.cmd_cnt != 1)
 		{
