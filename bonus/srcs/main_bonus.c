539d0f57871716835d30c6b94292eab8244e78d9
Pierre-marie LAM <plam@e2r4p8.clusters.42paris.fr>
2022-02-02 19:17:27 +0100

    still got the bnus exception, but set the infile & outfile use in the structure

--- bonus/srcs/main_bonus.c
+++ bonus/srcs/main_bonus.c
@@ -6,38 +6,37 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 09:33:09 by plam              #+#    #+#             */
-/*   Updated: 2022/02/02 17:06:55 by plam             ###   ########.fr       */
+/*   Updated: 2022/02/02 19:16:06 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
 
-void	init_open(int ac, t_ppx ppx, int infile, int outfile)
+void	init_open(int ac, t_ppx *ppx)
 {
-	infile = open_file(ppx.av[1], 2);
-	outfile = open_file(ppx.av[ac - 1], 3);
+	ppx->infile = open_file(ppx->av[1], 2);
+	ppx->outfile = open_file(ppx->av[ac - 1], 3);
+	if (ppx->infile == -1)
+		close(ppx->pipe[R_END]);
 }
 
 int	main(int ac, char **av, char **envp)
 {
 	t_ppx	ppx;
 	int		i;
-	int		infile;
-	int		outfile;
 
 	init_ppx(&ppx, ac, av, envp);
 	if (ppx.ac >= 5)
 	{
 		i = 2;
-		infile = open_file(ppx.av[1], 2);
-		outfile = open_file(ppx.av[ac - 1], 3);
-		dup2(infile, STDIN_FILENO);
+		init_open(ac, &ppx);
+		dup2(ppx.infile, STDIN_FILENO);
 		while (ppx.cmd_cnt != 1)
 		{
 			child_process(av[i++], envp, &ppx);
 			ppx.cmd_cnt--;
 		}
-		dup2(outfile, STDOUT_FILENO);
+		dup2(ppx.outfile, STDOUT_FILENO);
 		cmd_exec(av[ac - 2], envp, &ppx);
 	}
 	else
